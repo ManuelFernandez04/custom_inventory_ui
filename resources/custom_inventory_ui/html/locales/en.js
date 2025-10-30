fab7f68427ab02b746d4d1fa6859462a9b3c54d5
Manuel Fernandez <116933728+ManuelFernandez04@users.noreply.github.com>
2025-10-29 23:00:29 -0300

    Add files via upload

---
+++ resources/custom_inventory_ui/html/locales/en.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Drop";
+invLocale.useItem = "Usar";
+invLocale.giveItem = "Dar";
+invLocale.secondInventoryNotAvailable = "El segundo inventario no está disponible";
