5ada0b17e50acf4d127027fdb0b2bda6f04c0877
Manuel Fernandez <116933728+ManuelFernandez04@users.noreply.github.com>
2025-10-29 23:02:12 -0300

    Delete resources/custom_inventory_ui directory

--- resources/custom_inventory_ui/README.md
+++
@@ -1,3 +0,0 @@
-Go ahead and feel free to visit this page to find out what we have to offer!
-
-https://www.patreon.com/user?u=19053829
--- resources/custom_inventory_ui/html/locales/en.js
+++
@@ -1,5 +0,0 @@
-var invLocale = new Object();
-invLocale.dropItem = "Drop";
-invLocale.useItem = "Usar";
-invLocale.giveItem = "Dar";
-invLocale.secondInventoryNotAvailable = "El segundo inventario no está disponible";
